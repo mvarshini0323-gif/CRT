7b421060fadbc71bed21e7369f7bc8e96cdaa458
Varshini M <mvarshini0323@gmail.com>
2026-03-13 11:16:12 +0530

    A

---
+++ IV-SEM/Python/Practice/M03_Core_Concepts/S02/PS02_Python_Collections.py
@@ -0,0 +1,19 @@
+# 3) creating  list with repeated elements 
+a = [1,2,3]
+print(a*2)
+
+# 4) Adding elements to alist 
+a =[1,2,3]
+a.append(50)
+a.insert(1,20)
+print(a)
+
+# 5) Removing Elements from list 
+b = list((1,23,4,5,7,98))
+print(b)
+b.remove(7)
+print(b)
+b.pop()
+print(b)
+b.clear()
+print(b)
